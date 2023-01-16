e011837f25d29caf516f17a13a5c1c9cca365530
darsh <98086747+dm1010@users.noreply.github.com>
2023-01-16 22:39:41 +0530

    Practical-3

--- Practical-3/Reverse the given input string/jscript.js
+++ Practical-3/Reverse the given input string/jscript.js
@@ -1,6 +1,6 @@
 function reverseInput() {
-    let inputBox = document.getElementById("inputBox");
-    let input = inputBox.value;
-    let reversedInput = input.split('').reverse().join('');
-    document.getElementById("output").textContent = reversedInput;
-  }
+  let inputBox = document.getElementById("inputBox");
+  let input = inputBox.value;
+  let reversedInput = input.split('').reverse().join('');
+  document.getElementById("output").textContent = reversedInput;
+}
