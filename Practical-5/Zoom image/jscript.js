996e3ee941c9686083c7444db8dd278be8b25c6d
darsh <98086747+dm1010@users.noreply.github.com>
2023-01-16 22:44:57 +0530

    Practical-5

---
+++ Practical-5/Zoom image/jscript.js
@@ -0,0 +1,8 @@
+function zoomin() {
+    var imgObj = document.getElementById("idImg");
+    imgObj.style.width = 1000 + "px";
+}
+function zoomout() {
+    var imgObj = document.getElementById("idImg");
+    imgObj.style.width = 500 + "px";
+}
